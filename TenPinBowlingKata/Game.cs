c3ab81a741c0f16aa2f502a0f3bd6bb855b733d7
James Brown <jamesdbrown79@gmail.com>
2017-07-04 15:20:11 +0100

    Update1

---
+++ TenPinBowlingKata/Game.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenPinBowlingKata
+{
+    public class Game
+    {
+        private int currentRoll = 0;
+        private int[] rolls = new int[21];
+
+        public void Roll(int pins)
+        {
+            rolls[currentRoll++] = pins;
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int frameIndex = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(frameIndex))  
+                {
+                    score += StrikeBonus(frameIndex);
+                    frameIndex++;
+                }
+                else if (IsSpare(frameIndex)) 
+                {
+                    score += SpareBonus(frameIndex);
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += FrameTotal(frameIndex);
+                    frameIndex += 2;
+                }
+            }
+            return score;
+        }
+
+        private bool IsSpare(int frameIndex)
+        {
+            return (rolls[frameIndex] + rolls[frameIndex + 1] == 10);
+        }
+
+        private bool IsStrike(int frameIndex)
+        {
+            return (rolls[frameIndex] == 10);
+        }
+
+        private int SpareBonus(int frameIndex)
+        {
+            return 10 + rolls[frameIndex + 2];
+        }
+
+        private int StrikeBonus(int frameIndex)
+        {
+            return 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+        }
+
+        private int FrameTotal(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1];
+        }
+    }
+}
---
+++ TenPinBowlingKataTests/GameTests.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TenPinBowlingKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenPinBowlingKata.Tests
+{
+    [TestClass()]
+    public class GameTests
+    {
+        private Game game;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            //setup
+            game = new Game();
+        }
+
+        private void rollMultiple(int numberOfBalls, int pins)
+        {
+            for (int index = 0; index < numberOfBalls; index++)
+            {
+                game.Roll(pins);
+            }
+        }
+
+        private void rollSpare()
+        {
+            game.Roll(5);
+            game.Roll(5); 
+        }
+
+        private void rollStrike()
+        {
+            game.Roll(10);
+        }
+
+        //Kata test 1
+        [TestMethod()]
+        public void should_return_gutter_game_scores_0()
+        {
+            //setup
+
+            //act
+            rollMultiple(20, 0);
+
+            //assert
+            Assert.AreEqual(0, game.Score());
+        }
+
+        //Kata test 2
+        [TestMethod()]
+        public void should_return_one_pin_per_frame_scores_20()
+        {
+            //setup
+
+            //act
+            rollMultiple(20, 1);
+
+            //assert
+            Assert.AreEqual(20, game.Score());
+        }
+
+        //Kata test 3
+        [TestMethod()]
+        public void should_return_spare_then_three_pins_then_all_misses_scores_16()
+        {
+            //setup
+
+            //act
+            rollSpare();
+            game.Roll(3);
+            rollMultiple(17, 0);
+
+            //assert
+            Assert.AreEqual(16, game.Score());
+        }
+
+        //Kata test 4
+        [TestMethod()]
+        public void should_return_strike_then_three_pins_then_four_pins_then_all_misses_scores_24()
+        {
+            //setup
+
+            //act
+            rollStrike();
+            game.Roll(3);
+            game.Roll(4);
+            rollMultiple(16, 0);
+
+            //assert
+            Assert.AreEqual(24, game.Score());
+        }
+
+        //Kata test 5
+        [TestMethod()]
+        public void should_return_perfect_game_scores_300()
+        {
+            //setup
+
+            //act
+            rollMultiple(12, 10);
+
+            //assert
+            Assert.AreEqual(300, game.Score());
+        }
+    }
+}
